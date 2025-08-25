cb4921a743d1eaa57a24b4f48766ef33809c9d71
James Marques <jamzmarks@gmail.com>
2025-08-25 00:03:54 -0300

    first commit

--- src/auth.ts
+++ src/auth.ts
@@ -3,4 +3,18 @@ import GitHub from "next-auth/providers/github"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [GitHub],
+  cookies: {
+    sessionToken: {
+      name: process.env.NODE_ENV === "production"
+        ? "__Secure-authjs.session-token"
+        : "authjs.session-token",
+      options: {
+        httpOnly: true,
+        sameSite: "lax",
+        path: "/",
+        secure: process.env.NODE_ENV === "production", // só força HTTPS em prod
+      },
+    },
+  },
+
 })
